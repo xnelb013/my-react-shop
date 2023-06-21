03eeaa039ae0d94bc8c87defd8865f1a0d9dfb96
Eunseok Choi <xnelb013@naver.com>
2023-06-21 19:48:43 +0900

    FinalCommit

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRecoilValueLoadable } from "recoil";
 import { useNavigate } from "react-router-dom";
 import { fetchProductsByCategory } from "../store/products";
@@ -11,6 +11,7 @@ const SearchInput = React.forwardRef<HTMLInputElement>((props, ref) => {
   const productsLoadable = useRecoilValueLoadable(fetchProductsByCategory({ category: "all" }));
   const products = productsLoadable.state === "hasValue" ? productsLoadable.contents : [];
   const navigate = useNavigate();
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   // 소문자 변환
   const filteredProducts = products.filter((product) => product.title.toLowerCase().includes(searchText.toLowerCase()));
@@ -19,6 +20,21 @@ const SearchInput = React.forwardRef<HTMLInputElement>((props, ref) => {
   const handleProductClick = (id: number) => {
     navigate(`/product/${id}`);
   };
+
+  // 키보드로 리스트 움직이기
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "ArrowDown") {
+        setSelectedIndex((prevIndex) => Math.min(prevIndex + 1, filteredProducts.length - 1));
+      } else if (e.key === "ArrowUp") {
+        setSelectedIndex((prevIndex) => Math.max(prevIndex - 1, -1));
+      } else if (e.key === "Enter" && selectedIndex >= 0) {
+        handleProductClick(filteredProducts[selectedIndex].id);
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [filteredProducts, selectedIndex]);
 
   return (
     <div className={styled.container} ref={ref} {...props}>
@@ -33,10 +49,10 @@ const SearchInput = React.forwardRef<HTMLInputElement>((props, ref) => {
           className={`${styled.itemList} !fixed left-0 sm:!absolute sm:top-12 menu dropdown-content w-full sm:w-64 max-h-96 shadow text-base-content overflow-y-auto bg-white dark:bg-gray-600`}
         >
           {filteredProducts.length > 0 ? (
-            filteredProducts.map((product) => (
+            filteredProducts.map((product, index) => (
               <li
                 key={product.id}
-                className={`${styled.item} menu-item`}
+                className={`${styled.item} menu-item ${index === selectedIndex ? styled.selected : ""}`}
                 onClick={() => handleProductClick(product.id)}
               >
                 {product.title}
